15bc9ad6df48614d5a57eb4b3a25187693185e39
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-04-07 21:59:42 +0800

    Delete 判断年份是否闰年.py

--- 判断年份是否闰年.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-year = int(input('Please input your year: '))
-if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-    print('该年是闰年！')
-else:
-    print('该年不是闰年！')
-
