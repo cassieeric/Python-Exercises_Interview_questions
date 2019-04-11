82d9d89e88bb92ab1bee5201df707e9a4faead38
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-04-11 21:58:37 +0800

    Create 已知三角形的两边长及其夹角，求第三边长.py

---
+++ 基础题目/已知三角形的两边长及其夹角，求第三边长.py
@@ -0,0 +1,8 @@
+import math
+
+a = int(input('请输入三角形的一条边长：'))
+b = int(input('请输入三角形的另一条边长：'))
+C = int(input('请输入三角形的两条边长的夹角度数：'))
+# c = math.sqrt(a*a + b*b - 2*a*b*math.cos(C*math.pi/180))
+c = math.sqrt(a**2 + b**2 - 2*a*b*math.cos(C*math.pi/180))
+print(c)
