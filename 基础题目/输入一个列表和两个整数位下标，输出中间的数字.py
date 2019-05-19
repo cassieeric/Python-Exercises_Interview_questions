632cfe155d8b742245179b2b444285ca49a20c2b
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-19 15:25:52 +0800

    Create 输入一个列表和两个整数位下标，输出中间的数字.py

---
+++ 基础题目/输入一个列表和两个整数位下标，输出中间的数字.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# a = [1, 2, 3, 4, 5, 6]
+# b = 2
+# c = 5
+# d = a[b: c+1]
+# print(d)
+
+a = list(input('Please input a list: '))
+b = int(input('Please input a number: '))
+c = int(input('Please input a number: '))
+print(a)
+
+if b < len(a) and c < len(a):
+    if b < c:
+        print(a[b: c+1])
+    else:
+        print('error!! b must be small to c')
+else:
+    print('The number is out of list! Please input your number again.')
