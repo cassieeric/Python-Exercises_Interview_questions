f1a69fd58237798afd42cc3ba8337d981e1c438e
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-28 21:53:30 +0800

    Update quick_sort快速排序ver2.py

--- 简单算法题/排序算法/quick_sort快速排序ver2.py
+++ 简单算法题/排序算法/quick_sort快速排序ver2.py
@@ -1,5 +1,22 @@
 # -*- coding: utf- 8 -*-
 
+'''
+说起快排的Python实现，首先谈一下，快速排序的思路：
+
+1、取一个参考值放到列表中间，初次排序后，让左侧的值都比他小，右侧的值，都比他大。
+
+2、分别对左侧和右侧的部分递归第1步的操作
+
+实现思路：
+
+两个指针left，right分别指向列表的第一个元素和最后一个元素，然后取一个参考值，默认为第一个列表的第一个元素list[0]，称为K
+然后left指向的值先和参考值K进行比较，若list[left]小于或等于K值，left就一直向右移动，left+1，直到移动到大于K值的地方，停住
+right指向的值和参考值K进行比较，若list[right]大于K值，right就一直向左移动，right-1，直到移动到小于K值的地方，停住
+此时，left和right若还没有相遇，即left还小于right，则二者指向的值互换
+若已经相遇则说明，第一次排序已经完成，将list[right]与list[0]的值进行互换，进行之后的递归。
+
+'''
+
 #快排的主函数，传入参数为一个列表，左右两端的下标
 def QuickSort(list,low,high):
     if high > low:
@@ -36,3 +53,5 @@ list_demo = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8]
 print(list_demo)
 QuickSort(list_demo,0,9)
 print(list_demo)
+
+# 参考链接：https://www.cnblogs.com/kunpengv5/p/7833361.html
