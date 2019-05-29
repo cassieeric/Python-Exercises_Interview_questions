d6f2a19a84e9bd92a3fc9224e5bc10b23b285e7f
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-29 15:38:55 +0800

    Add files via upload

---
+++ 简单算法题/排序算法/快速排序/quick_sort快速排序ver1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf- 8 -*-
+
+def find_middle_value(list, leftindex, rightindex):
+    if leftindex < rightindex:
+        i = leftindex
+        j = rightindex
+        temp = list[i]
+        while i < j:
+            while i < j and temp <= list[j]:
+                j = j - 1
+            list[i] = list[j]
+            while i < j and temp >= list[i]:
+                i = i + 1
+            list[j] = list[i]
+
+        list[j] = temp
+        middleIndex = i
+        return middleIndex
+
+    # if leftindex < rightindex:
+    #     i = leftindex
+    #     j = rightindex
+    #     temp = list[i]
+    #     while i < j:
+    #         # while i < j and list[j] >= temp:
+    #         while i < j and temp <= list[j]:
+    #             j = j - 1
+    #         list[i] = list[j]
+    #         while i < j and temp >= list[i]:
+    #             i = i + 1
+    #         list[j] = list[i]
+    #     list[j] = temp
+    #     middleIndex = i
+    #     return middleIndex
+
+
+def quicksort(list, leftindex, rightindex):
+    if leftindex < rightindex:
+        temp = find_middle_value(list, leftindex, rightindex)
+        quicksort(list, leftindex, temp-1)
+        quicksort(list, temp+1, rightindex)
+
+if __name__ == '__main__':
+    list1 = [2, 8, 7, 1, 3, 5, 6, 4]
+    print(list1)
+    quicksort(list1, 0, len(list1)-1)
+    print(list1)
