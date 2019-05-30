d1b9475b74b060fe1141aa083acf793a4fae6623
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-30 10:56:08 +0800

    Update 冒泡排序.py

--- 简单算法题/排序算法/冒泡排序/冒泡排序.py
+++ 简单算法题/排序算法/冒泡排序/冒泡排序.py
@@ -7,8 +7,8 @@ def bubble_sort(list):
                 temp = list[i]
                 list[i] = list[j]
                 list[j] = temp
-    print(list)
 
 if __name__ == '__main__':
     list1 = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8]
     bubble_sort(list1)
+    print(list1)
