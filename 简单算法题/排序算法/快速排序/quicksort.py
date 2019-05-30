7ba4da82e5f95f9612c6a1e6ef094763187fe290
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-30 09:32:15 +0800

    Update quicksort.py

--- 简单算法题/排序算法/快速排序/quicksort.py
+++ 简单算法题/排序算法/快速排序/quicksort.py
@@ -27,3 +27,11 @@ if __name__ == '__main__':
     print(list1)
     quick_sort(list1, 0, len(list1)-1)
     print(list1)
+
+'''
+    总结：
+        1、快速排序的关键点在分解函数这块，很容易让人混淆。
+        2、记得partitions函数里边用的是三个while，如果使用if语句，则会出现排序不完整的情况。
+        3、在判断temp与list值对比的时候，记得先从右往左进行(while i < j and temp <= list[j]:)，
+        如果将(while i < j and temp >= list[i]:)放在前面的话，则排序会不完整，无法达到预期的效果。
+'''
