788cbbf8724a0bea12a532f5f05e8f92166b4c24
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-30 09:35:08 +0800

    Update quicksort.py

--- 简单算法题/排序算法/快速排序/quicksort.py
+++ 简单算法题/排序算法/快速排序/quicksort.py
@@ -34,4 +34,7 @@ if __name__ == '__main__':
         2、记得partitions函数里边用的是三个while，如果使用if语句，则会出现排序不完整的情况。
         3、在判断temp与list值对比的时候，记得先从右往左进行(while i < j and temp <= list[j]:)，
         如果将(while i < j and temp >= list[i]:)放在前面的话，则排序会不完整，无法达到预期的效果。
+        4、从整体上来看，partitions函数中进行了三次交换，其中相对不明显的是temp = list[i]，list[i] = list[j]
+        与list[j] = temp，由于相隔的距离有些远，不太容易被理解，这个细节需要注意一下。大概意思是如果i不再小于j的时候，
+        说明i和j相遇了，此时将list[i]和list[j]的值进行互换，返回j即可。
 '''
