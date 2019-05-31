f05d923d81131e4223bf6eea0970b9cd183d9669
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-31 10:32:07 +0800

    Create 100以内能被7整除但是不能同时被5整除的所有整数.py

---
+++ 简单算法题/数值计算/100以内能被7整除但是不能同时被5整除的所有整数.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+for i in range(1, 101):
+
+    if i % 7 == 0 and i % 5 != 0:
+
+        print(i)
+
+
+
+i = 0
+
+while i < 100:
+
+    i += 1
+
+    if i % 7 == 0 and i % 5 != 0:
+
+        print(i)
