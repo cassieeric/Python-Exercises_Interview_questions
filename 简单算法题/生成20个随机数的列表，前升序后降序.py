a61040095e3804745850ddc7d1e1210284330cd2
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-06 21:31:20 +0800

    Delete 生成20个随机数的列表，前升序后降序.py

--- 简单算法题/生成20个随机数的列表，前升序后降序.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import random
-number = [random.randint(1, 100) for i in range(20)]
-print(number)
-former = sorted(number[0:10])
-later = sorted(number[10:20], reverse=True)
-print(former)
-print(later)
