e3d0fe05354341b08103227933f3adf1e9221035
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-06 21:32:23 +0800

    Create 计算10以内的奇数.py

---
+++ 简单算法题/数值计算/计算10以内的奇数.py
@@ -0,0 +1,12 @@
+# -coding: utf-8 -*-
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+i = 0
+while i < 10:
+    i += 1
+    if i % 2 == 0:
+        continue
+    print(i)
