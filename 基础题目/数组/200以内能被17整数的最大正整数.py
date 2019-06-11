f972221982a5d7fab3f6707e84bdba1dc6c92581
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-11 23:14:59 +0800

    Create 200以内能被17整数的最大正整数.py

---
+++ 基础题目/数组/200以内能被17整数的最大正整数.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# a = []
+# for i in range(1, 200):
+#     if i % 17 == 0:
+#         # print(i)
+#         a.append(i)
+# print(max(a))
+
+
+for i in range(200, 1, -1):
+    if i % 17 == 0:
+        print(i)
+        break
