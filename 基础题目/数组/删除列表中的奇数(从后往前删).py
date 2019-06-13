da64d53aeba8cb15b8e913b98b11e791a5523fc5
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-13 21:11:04 +0800

    Create 删除列表中的奇数(从后往前删).py

---
+++ 基础题目/数组/删除列表中的奇数(从后往前删).py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# import random
+#
+# a = [random.randint(1, 100) for i in range(10)]
+# print(a)
+# j = len(a) - 1
+# while j >= 0:
+#     if a[j] % 2 != 0:
+#         print(a[j])
+#         del a[j]
+#     j -= 1
+# print(a)
+
+# print('偶数: ', even)
