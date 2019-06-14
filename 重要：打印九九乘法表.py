ab6ac9570b060fb68c9267a969fd7af81c63fb7a
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-14 21:24:09 +0800

    Delete 重要：打印九九乘法表.py

--- 重要：打印九九乘法表.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-for i in range(1, 10):
-    # print('\t')
-    for j in range(1, i+1):
-    # for j in range(1, 10):
-        print('%s*%s=' % (i, j), i * j, end='\t')
-    print('\n')  # 这个地方务必要加上
-        # print('%s*%s=' % (i, j), i * j, end='\t')
-        # print('\t')
